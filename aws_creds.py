c887561145dafda417717256b9691809e3e1ad79
Oliver <kollo.oliver@gmail.com>
2022-03-10 17:56:59 +0000

    Added a gitignore file with the aws_credentials saved inside

--- aws_creds.py
+++
@@ -1,7 +0,0 @@
-DATABASE_TYPE = 'postgresql'
-DBAPI = 'psycopg2'
-HOST = 'ftx-scraper2.cn4izzmm7yyd.eu-west-2.rds.amazonaws.com' 
-USER = 'postgres'
-PASSWORD = '8750Ironpineapple?'
-PORT = 5432
-DATABASE = 'postgres'
