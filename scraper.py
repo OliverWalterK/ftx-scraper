515973b49a19a45dec1c24d0410a4bbc759bec4a
Oliver <kollo.oliver@gmail.com>
2022-03-16 20:53:20 +0000

    minor changes

--- scraper.py
+++ scraper.py
@@ -24,19 +24,12 @@ class FtxScraper:
         This is the webdriver object.
     '''
     def __init__(self, url:str, options=None):
-        self.url = url
-        self.all_url = []
-        self.valid_url = []
-        self.unique_id = str(uuid.uuid4())
-        self.current_time = datetime.datetime.now()
         # self.options = Options()
         # self.options.add_argument('--headless')
         if options:
             self.driver = Chrome(ChromeDriverManager().install(), options=options)
         else:
             self.driver = Chrome(ChromeDriverManager().install())
-        self.driver.maximize_window()
-        self.driver.get(self.url)
 
         DATABASE_TYPE = aws_creds.DATABASE_TYPE
         DBAPI = aws_creds.DBAPI
@@ -45,8 +38,32 @@ class FtxScraper:
         PASSWORD = aws_creds.PASSWORD
         PORT = aws_creds.PORT
         DATABASE = aws_creds.DATABASE
+
+        self.url = url
+        self.all_url = []
+        self.valid_url = []
+        self.unique_id = str(uuid.uuid4())
+        self.current_time = datetime.datetime.now()
         self.engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
         self.client = boto3.client('s3')
+        self.crypto_dictionary = {
+                                    'UUID':[],
+                                    'Link':[],
+                                    'Name':[],
+                                    'Price':[],
+                                    'Time':[],
+                                    #'Percentage increase':[]
+                                } 
+        self.global_dictionary = {
+                                        'UUID':[],
+                                        'Link':[],
+                                        'Name':[],
+                                        'Price':[],
+                                        'Time':[],
+                                        #'Percentage increase':[]
+                                    } 
+        self.driver.maximize_window()
+        self.driver.get(self.url)
     
     def find_all_links(self):
         '''
@@ -97,65 +114,51 @@ class FtxScraper:
         screenshots: png
             A screenshot of the last 24 hours market value.
         '''
-        crypto_dictionary = {
-                    'UUID':[],
-                    'Link':[],
-                    'Name':[],
-                    'Price':[],
-                    'Time':[]
-                                } 
-        df_global_dictionary = {
-                        'UUID':[],
-                        'Link':[],
-                        'Name':[],
-                        'Price':[],
-                        'Time':[]
-                                    } 
         count = 0
-        for links in self.valid_url:
+        for links in self.valid_url[:3]:
             crypto_name = links.split("/")[-1]
             self.driver.get(links)
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "/")))   
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//h5[@class='MuiTypography-root MuiTypography-h5']")))   
             time.sleep(1)
             try:
-                crypto_dictionary['UUID'].append(self.unique_id)
-                df_global_dictionary['UUID'].append(self.unique_id)
+                self.crypto_dictionary['UUID'].append(self.unique_id)
+                self.global_dictionary['UUID'].append(self.unique_id)
             except NoSuchElementException:
-                crypto_dictionary['UUID'].append('N/A')
-                df_global_dictionary['UUID'].append('N/A')
+                self.crypto_dictionary['UUID'].append('N/A')
+                self.global_dictionary['UUID'].append('N/A')
             try:
-                value = self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div/main/div[3]/div[3]/div/div/div/span[1]/p[2]').text
-                crypto_dictionary['Price'].append(value)
-                df_global_dictionary['Price'].append(value)
+                value = self.driver.find_element(By.XPATH, "//h5[@class='MuiTypography-root MuiTypography-h5']").text
+                self.crypto_dictionary['Price'].append(value)
+                self.global_dictionary['Price'].append(value)
             except NoSuchElementException:
-                crypto_dictionary['Price'].append('N/A')
-                df_global_dictionary['Price'].append('N/A')
+                self.crypto_dictionary['Price'].append('N/A')
+                self.global_dictionary['Price'].append('N/A')
             try:
-                crypto_dictionary['Link'].append(links)
-                df_global_dictionary['Link'].append(links)
+                self.crypto_dictionary['Link'].append(links)
+                self.global_dictionary['Link'].append(links)
             except NoSuchElementException:
-                crypto_dictionary['Link'].append('N/A')
-                df_global_dictionary['Link'].append('N/A')
+                self.crypto_dictionary['Link'].append('N/A')
+                self.global_dictionary['Link'].append('N/A')
             try:
-                crypto_dictionary['Name'].append(crypto_name)
-                df_global_dictionary['Name'].append(crypto_name)
+                self.crypto_dictionary['Name'].append(crypto_name)
+                self.global_dictionary['Name'].append(crypto_name)
             except NoSuchElementException:
-                crypto_dictionary['Name'].append('N/A')
-                df_global_dictionary['Name'].append('N/A')
-            crypto_dictionary['Time'].append(self.current_time.strftime("%c"))
-            df_global_dictionary['Time'].append(self.current_time.strftime("%c"))
+                self.crypto_dictionary['Name'].append('N/A')
+                self.global_dictionary['Name'].append('N/A')
+            self.crypto_dictionary['Time'].append(self.current_time.strftime("%c"))
+            self.global_dictionary['Time'].append(self.current_time.strftime("%c"))
             count = count + 1
             print(f'Downloading data:(json) and taking screenshot:(png) for {crypto_name}, {count}/{len(self.valid_url)}.')
             if not os.path.exists(f'./raw_data/{crypto_name}'):
                 os.makedirs(f'./raw_data/{crypto_name}')
             with open(f'./raw_data/{crypto_name}/{self.current_time}.json', 'w') as fp:
-                json.dump(crypto_dictionary, fp)
+                json.dump(self.crypto_dictionary, fp)
             try:
                 self.driver.save_screenshot(f'./raw_data/{crypto_name}/{self.current_time}.png')
             except NoSuchElementException:
                 print(f"No screenshot was made for {crypto_name}.")
-        df_global_dictionary = pd.DataFrame(df_global_dictionary)
-        df_global_dictionary.to_csv('~/Desktop/AiCore/Scraper/ftx-scraper/raw_data/dataframe.csv', index = False)
+        dataframe = pd.DataFrame(self.global_dictionary)
+        dataframe.to_csv('~/Desktop/AiCore/Scraper/ftx-scraper/raw_data/dataframe.csv', index = False)
     def upload_data(self):
         '''
         This method will loop through the valid_url list and extract the price, name, and link for every cryptocurrency. 
@@ -171,53 +174,39 @@ class FtxScraper:
             A screenshot of the last 24 hours market value.
 
         '''
-        crypto_dictionary = {
-                    'UUID':[],
-                    'Link':[],
-                    'Name':[],
-                    'Price':[],
-                    'Time':[]
-                                } 
-        global_dictionary = {
-                        'UUID':[],
-                        'Link':[],
-                        'Name':[],
-                        'Price':[],
-                        'Time':[]
-                                    } 
         count = 0
         for links in self.valid_url[:4]:
             self.driver.get(links)
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "/")))   
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//h5[@class='MuiTypography-root MuiTypography-h5']")))   
             time.sleep(1)
             crypto_name = links.split("/")[-1]
             try:
-                crypto_dictionary['UUID'].append(self.unique_id)
-                global_dictionary['UUID'].append(self.unique_id)
+                self.crypto_dictionary['UUID'].append(self.unique_id)
+                self.global_dictionary['UUID'].append(self.unique_id)
             except NoSuchElementException:
-                crypto_dictionary['UUID'].append('N/A')
-                global_dictionary['UUID'].append('N/A')
+                self.crypto_dictionary['UUID'].append('N/A')
+                self.global_dictionary['UUID'].append('N/A')
             try:
-                value = self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div/main/div[3]/div[3]/div/div/div/span[1]/p[2]').text
-                crypto_dictionary['Price'].append(value)
-                global_dictionary['Price'].append(value)
+                value = self.driver.find_element(By.XPATH, "//h5[@class='MuiTypography-root MuiTypography-h5']").text
+                self.crypto_dictionary['Price'].append(value)
+                self.global_dictionary['Price'].append(value)
             except NoSuchElementException:
-                crypto_dictionary['Price'].append('N/A')
-                global_dictionary['Price'].append('N/A')
+                self.crypto_dictionary['Price'].append('N/A')
+                self.global_dictionary['Price'].append('N/A')
             try:
-                crypto_dictionary['Link'].append(links)
-                global_dictionary['Link'].append(links)
+                self.crypto_dictionary['Link'].append(links)
+                self.global_dictionary['Link'].append(links)
             except NoSuchElementException:
-                crypto_dictionary['Link'].append('N/A')
-                global_dictionary['Link'].append('N/A')
+                self.crypto_dictionary['Link'].append('N/A')
+                self.global_dictionary['Link'].append('N/A')
             try:
-                crypto_dictionary, global_dictionary['Name'].append(crypto_name)
-                global_dictionary['Name'].append(crypto_name)
+                self.crypto_dictionary['Name'].append(crypto_name)
+                self.global_dictionary['Name'].append(crypto_name)
             except NoSuchElementException:
-                crypto_dictionary['Name'].append('N/A')
-                global_dictionary['Name'].append('N/A')
-            crypto_dictionary['Time'].append(self.current_time.strftime("%c"))
-            global_dictionary['Time'].append(self.current_time.strftime("%c"))
+                self.crypto_dictionary['Name'].append('N/A')
+                self.global_dictionary['Name'].append('N/A')
+            self.crypto_dictionary['Time'].append(self.current_time.strftime("%c"))
+            self.global_dictionary['Time'].append(self.current_time.strftime("%c"))
             count = count + 1
             print(f'Uploading data:(json) and screenshot:(png) for {crypto_name}, {count}/{len(self.valid_url)}.')
             with tempfile.TemporaryDirectory() as tmpdirname:
@@ -227,10 +216,10 @@ class FtxScraper:
                 except NoSuchElementException:
                     print(f"No screenshot was made for {crypto_name}!")
                 with open(tmpdirname + f'/{crypto_name}.json', 'w') as fp:
-                    json.dump(crypto_dictionary, fp)
+                    json.dump(self.crypto_dictionary, fp)
                 self.client.upload_file(tmpdirname + f'/{crypto_name}.json', 'ftx-scraper', f'{crypto_name}_{self.current_time.strftime("%c")}.json')
-        df_global_dictionary = pd.DataFrame(global_dictionary)
-        df_global_dictionary.to_sql('df_global_dictionary', con=self.engine, if_exists='append', index=False)
+        dataframe = pd.DataFrame(self.global_dictionary)
+        dataframe.to_sql('ftx-dataframe', con=self.engine, if_exists='append', index=False)
 
 if __name__ == '__main__':
     bot = FtxScraper()
